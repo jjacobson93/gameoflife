8971ebfb7788b6d5687a53d2f9faabeb87302c8c
Jeremy Jacobson <jjacobson93@gmail.com>
2014-04-04 22:34:23 -0700

    Cleaned up code. Iteration and graphics work now.

--- main.ml
+++ main.ml
@@ -1,121 +1,115 @@
 #load "graphics.cma"
 #load "unix.cma"
-#directory "+threads"
-#load "threads.cma";;
 
-Graphics.open_graph " 640x480";;
-
+(* matrix helper functions *)
 let matrix = Array.make_matrix;;
+let matrix_copy m = Array.map Array.copy m;; (* copies 2d array (matrix) *)
 
 let square_size = 15;;
-let frame_width = Graphics.size_x ();;
-let frame_height = Graphics.size_y ();;
 
-(* let universe = matrix (frame_height/square_size) (frame_width/square_size) 0;; *)
-
-
-let universe =
-	[|
-	[|0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0|];
-	[|0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0|];
-	[|0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 0; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0|];
-	[|0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 1; 0; 0; 0; 0; 0; 0; 1; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 1; 0|];
-	[|0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 0; 0; 0; 1; 0; 0; 0; 0; 1; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 1; 0|];
-	[|0; 1; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 0; 0; 0; 0; 0; 1; 0; 0; 0; 1; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0|];
-	[|0; 1; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 0; 0; 0; 1; 0; 1; 1; 0; 0; 0; 0; 1; 0; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0|];
-	[|0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 0; 0; 0; 0; 0; 1; 0; 0; 0; 0; 0; 0; 0; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0|];
-	[|0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 0; 0; 0; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0|];
-	[|0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0|];
-	[|0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0|]
-	|];;
-
-(* let draw_grid _ =
-	for i = 0 to 150/square_size do
-		for j = 0 to 150/square_size do
-			let x = i*square_size and
-			y = j*square_size in
-				Graphics.draw_rect x y square_size square_size
+let random m n uni = 
+	Random.self_init();
+	for i = 0 to m do
+		for j = 0 to n do
+			uni.(i).(j) <- Random.int 2
 		done
-	done;; *)
+	done;;
+	
 
+let glider_gun = [(1, 26); (2, 24); (2, 26); (3, 14); (3, 15);
+	(3, 22); (3, 23); (3, 36); (3, 37); (4, 13); (4, 17); 
+	(4, 22); (4, 23); (4, 36); (4, 37); (5, 2); (5, 3); 
+	(5, 12); (5, 18); (5, 22); (5, 23); (6, 2); (6, 3); 
+	(6, 12); (6, 16); (6, 18); (6, 19); (6, 24); (6, 26); 
+	(7, 12); (7, 18); (7, 26); (8, 13); (8, 17); (9, 14); (9, 15)];;
+
+(* Set cells from a list of tuples *)
+let set_cells cells uni =
+	List.iter (fun (i, j) -> uni.(i).(j) <- 1) cells;;
+
+(* the core of the game: checks if cell i,j is alive or dead *)
 let live_or_die i j uni =
-	let neighbors = 
-		(if i > 0 && j > 0 then uni.(i - 1).(j - 1) else 0) +
-		(if i > 0 then uni.(i - 1).(j) else 0) +
-		(if i > 0 && (j + 1) < (Array.length uni.(i - 1)) then uni.(i - 1).(j + 1) else 0) +
-		(if j > 0 then uni.(i).(j - 1) else 0) +
-		(if (j + 1) < (Array.length uni.(i)) then uni.(i).(j + 1) else 0) +
-		(if j > 0 && (i + 1) < (Array.length uni) then uni.(i + 1).(j - 1) else 0) +
-		(if (i + 1) < (Array.length uni) then uni.(i + 1).(j) else 0) +
-		(if (i + 1) < (Array.length uni) && (j + 1) < (Array.length uni.(i)) then uni.(i + 1).(j + 1) else 0)
-		and
-		current = uni.(i).(j) in
-			(* Printf.printf "(%d, %d) %d has neighbors = %d\n" i j current neighbors; *)
-			if current == 1 then
-				if neighbors == 2 || neighbors == 3 then 1 else 0
-			else if current == 0 && neighbors == 3 then 1
-			else 0;;
+	let m = Array.length uni and
+		n = Array.length uni.(i) in
+		let neighbors =
+			let alive x y = 
+				if x >= 0 && y >= 0 && x < m && y < n then 
+						(* Printf.printf "(%d, %d) is alive? %d\n" x y uni.(x).(y); *)
+						uni.(x).(y)
+				else 0 in
+				(alive (i - 1) (j - 1)) +
+				(alive (i - 1) j) +
+				(alive (i - 1) (j + 1)) +
+				(alive i (j - 1)) +
+				(alive i (j + 1)) +
+				(alive (i + 1) (j - 1)) +
+				(alive (i + 1) j) +
+				(alive (i + 1) (j + 1)) in
 
-let minisleep (sec: float) =
-    ignore (Unix.select [] [] [] sec);;
-		
+				let current = uni.(i).(j) in
+				if current == 1 && neighbors < 2 
+					then 0 (* dies of loneliness *)
+				else if current == 1 && neighbors > 3 
+					then 0 (* dies of overcrowding *)
+				else if current == 1 && (neighbors == 2 || neighbors == 3)
+					then 1 (* stays alive *)
+				else if current == 0 && neighbors == 3
+					then 1 (* born from 3 neighbors *)
+				else 0;; (* stays dead *)
 
-let rec draw_grid uni = 
-	(* Unix.sleep 1; *)
-	(* Initialize indexes *)
-	let i = ref ((Array.length uni) - 1) and j = ref 0 and
-		new_universe = Array.copy uni in
-	let f e = 
-		let g h =
-			(* This is the fancy stuff right here *)
-			(* h is an integer in the array *)
-			let x = !j * square_size and y = !i * square_size in
-				Graphics.set_color Graphics.black;
-				Graphics.draw_rect x y square_size square_size;
-				if h == 1 then begin
-					Graphics.set_color Graphics.blue;
-					Graphics.fill_rect x y square_size square_size;
-				end;
-				let state = live_or_die !i !j uni in
-					(* Printf.printf "%d, %d\n" !i !j; *)
-					new_universe.(!i).(!j) <- state;
-				(* End fancy stuff *)
-				j := !j + 1;
-				in
-					(* Iterate through each element of the row *)
-					Array.iter g e;
-					j := 0;
-					i := !i - 1;
-					in
-						(* Iterate through each row of the universe *)
-						Array.iter f uni;
-						(* print_endline "Sleeping..."; *)
-						minisleep 0.5;
-						Graphics.clear_graph();
-						(* print_endline "Awake!"; *)
-						draw_grid new_universe;;
+(* Runs an iteration and returns a new universe *)
+let run_iteration uni =
+	let m = (Array.length uni) and 
+		n = (Array.length uni.(0)) and
+		new_uni = matrix_copy uni in
+		for x = 0 to m - 1 do
+			for y = 0 to n - 1 do
+				new_uni.(x).(y) <- live_or_die x y uni;
+			done
+		done;
+	new_uni;;
 
-(* let run_iteration _ = 
-	(* Initialize indexes *)
-	let i = ref 0 and j = ref 0 in
-	Graphics.set_color Graphics.blue;
-	let f e = 
-		let g h =
-			(* This is the fancy stuff right here *)
-			(* h is an integer in the array *)
-			let x = !i * square_size and y = !j * square_size in
-				if h == 1 then Graphics.fill_rect x y square_size square_size;
-				(* End fancy stuff *)
-				i := !i + 1; 
-				in
-					(* Iterate through each element of the row *)
-					Array.iter g e;
-					i := 0;
-					j := !j + 1; 
-					in
-						(* Iterate through each row of the universe *)
-						Array.iter f universe;; *)
+(* Draws grid with given universe *)
+let draw_grid uni = 
+	Graphics.clear_graph();
+	let m = (Array.length uni) and 
+		n = (Array.length uni.(0)) in
+		for j = 0 to n - 1 do
+			for i = 0 to m - 1 do
+				let x = j * square_size and
+					y = ((m - 1) - i) * square_size in
+					if uni.(i).(j) == 1 then begin
+						Graphics.set_color Graphics.blue;
+						Graphics.fill_rect x y square_size square_size; 
+					end
+			done
+		done;;
 
+(* main code *)
+(* Usage: life [option] [gun|random] width height *)
 
-draw_grid universe;;
-read_line();;
+let () = 
+	let argc = Array.length Sys.argv in
+	let opt = if argc > 1 then Sys.argv.(1) else "NONE" in
+	let file_name = if opt == "-t" && argc > 2 then
+		Sys.argv.(2) else "NONE" in
+
+	let width = if opt != "-t" && argc > 3 then
+		max 40 (int_of_string Sys.argv.(3))
+		else if opt == "-t" && argc > 4 then
+		max 40 (int_of_string Sys.argv.(4))
+		else 40 in
+	let height = if opt != "-t" && argc > 4 then
+		max 22 (int_of_string Sys.argv.(4))
+		else if opt == "-t" && argc > 5 then
+		max 22 (int_of_string Sys.argv.(5))
+		else 22 in
+
+	Graphics.open_graph (Printf.sprintf " %dx%d" (width * square_size) (height * square_size));
+	let universe = ref (matrix height width 0) in
+	set_cells glider_gun !universe;
+	while true do
+		draw_grid !universe;
+		Unix.sleep 1;
+		universe := matrix_copy (run_iteration !universe);
+	done;;
